a54af476e74df1c6b23065bfae176429016ad885
Alex A. Kwashie <alex.kwashie@hotmail.com>
2019-01-26 15:43:03 +0000

    Optimizing webcam window frame

--- faceId.py
+++ faceId.py
@@ -3,7 +3,7 @@ import cv2
 face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 #search haarcascades to use more detections
 
-img = cv2.imread("people.jpg")
+img = cv2.imread("black.jpg")
 
 #creat grayscale image
 gray_img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
@@ -14,11 +14,11 @@ re = cv2.resize(img, (int(img.shape[1]/3), int(img.shape[0]/3)))
 #add a face search area size
 faces = face_cascade.detectMultiScale(gray_img,
 scaleFactor = 1.05,
-minNeighbors = 12)
+minNeighbors = 5)
 
 #draw face detected area
 for x, y, w, h in faces:
-  img=cv2.rectangle(img,(x,y),(x+w,y+h),(0,225,0),2)
+    img=cv2.rectangle(img,(x,y),(x+w,y+h),(0,225,0),2)
 
 print(type(faces)) #<class 'numpy.ndarray'>
 print(faces)
@@ -26,5 +26,3 @@ print(faces)
 cv2.imshow("Gray", img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-
-
